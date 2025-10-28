a869eed5c0b6c104e7a395d3a67dba07fe83030f
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2025-10-28 16:42:12 -0400

    Added second version to look more centered

---
+++ Abra2.java
@@ -0,0 +1,20 @@
+public class Abra2 {
+
+    public static void main(String[] args) {
+        String abraKadabra = "ABRAKADABRA";
+        int charsToPrint = 1;
+        int spaces = abraKadabra.length(); 
+        abr(abraKadabra, charsToPrint, spaces);
+
+    }
+
+    public static void abr(String str, int charsToPrint, int spaces) {
+        if (charsToPrint > str.length()) {
+            return;
+        }
+        String empty = " ";
+        System.out.println(empty.repeat(spaces) + str.substring(0, charsToPrint) + empty.repeat(spaces));
+        abr(str, charsToPrint + 2, spaces - 1);
+    }
+    
+}
